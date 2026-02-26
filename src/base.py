6c9400e1337aaf22a97af7b231db5102b448a9f3
kazumakano <kazumakano@outlook.jp>
2026-02-27 07:44:50 +0900

    update base

--- src/base.py
+++ src/base.py
@@ -1,10 +1,11 @@
 from os import PathLike
 from typing import Any, Optional, Self
+import pytorch_optimizer as optim
 import torch
 from lightning import pytorch as L
+from lightning.pytorch.utilities.types import OptimizerLRSchedulerConfig
 from omegaconf import DictConfig
 from safetensors import torch as safetensors
-from torch import optim
 from torchtune import training
 
 
@@ -13,14 +14,25 @@ class BaseModule(L.LightningModule):
         super().__init__()
         self.save_hyperparameters(hparams)
 
-    def configure_optimizers(self) -> None:
+    def configure_optimizers(self) -> torch.optim.Optimizer | OptimizerLRSchedulerConfig:
         match self.hparams["opt"]:
             case "adam":
                 opt = optim.Adam(self.parameters(), lr=self.hparams["lr"])
             case "adamw":
-                opt = optim.AdamW(self.parameters(), lr=self.hparams["lr"])
+                if self.hparams["sch"] == "free":
+                    opt = optim.ScheduleFreeAdamW(self.parameters(), lr=self.hparams["lr"])
+                else:
+                    opt = optim.AdamW(self.parameters(), lr=self.hparams["lr"])
             case "sgd":
-                opt = optim.SGD(self.parameters(), lr=self.hparams["lr"])
+                if self.hparams["sch"] == "free":
+                    opt = optim.ScheduleFreeSGD(self.parameters(), lr=self.hparams["lr"])
+                else:
+                    opt = optim.SGD(self.parameters(), lr=self.hparams["lr"])
+            case "soap":
+                opt = optim.SOAP(self.parameters(), lr=self.hparams["lr"])
+            case _:
+                raise ValueError(f"unknown optimizer {self.hparams['opt']} was specified")
+
         match self.hparams["sch"]:
             case "warm_cos":
                 return {
@@ -30,16 +42,32 @@ class BaseModule(L.LightningModule):
                         "interval": "step"
                     }
                 }
-            case None:
+            case "free" | None:
                 return opt
 
-    def to_safetensors(self, file: str | PathLike, metadata: Optional[dict[str, str]] = None) -> None:
-        safetensors.save_model(self, file, metadata)
+    def on_train_epoch_start(self) -> None:
+        if self.hparams["sch"] == "free":
+            self.optimizers().optimizer.train()
+
+    def on_validation_epoch_start(self):
+        if self.hparams["sch"] == "free":
+            self.optimizers().optimizer.eval()
+
+    def on_test_start(self) -> None:
+        if self.hparams["sch"] == "free":
+            self.optimizers().optimizer.eval()
+
+    def on_predict_start(self):
+        if self.hparams["sch"] == "free":
+            self.optimizers().optimizer.eval()
+
+    def to_safetensors(self, path: PathLike, metadata: Optional[dict[str, str]] = None) -> None:
+        safetensors.save_model(self, path, metadata=metadata)
 
 class BasePredictor(L.LightningModule):
     @classmethod
-    def load_from_safetensors(cls, file: str | PathLike, hparams: dict[str, Any] | DictConfig, device: int | str | torch.device = "cpu", **kwargs: Any) -> Self:
-        self = cls(hparams, **kwargs)
-        safetensors.load_model(self, file)    # device argument does not work with lightning
+    def load_from_safetensors(cls, path: PathLike, hparams: dict[str, Any] | DictConfig, device: int | str | torch.device = "cpu", **kwargs: Any) -> Self:
+        self = cls(hparams=hparams, **kwargs)
+        safetensors.load_model(self, path)    # device argument does not work with lightning
         self = self.to(device=device).eval()
         return self
