addae7d843f29afb50fa21a5eb06d6e4555027ac
kazumakano <kazumakano@outlook.jp>
2026-03-08 12:46:16 +0900

    update preprocess

--- corvs/preprocess.py
+++ corvs/preprocess.py
@@ -8,16 +8,18 @@ from numpy.typing import ArrayLike, NDArray
 def seg_by_timeout(data: pd.DataFrame, timeout_in_sec: float) -> list[pd.DataFrame]:
     return [d for _, d in data.groupby(by=(data["time"].diff() > timeout_in_sec).cumsum(), sort=False)]
 
-def pos_to_spd(pos: ArrayLike, freq_in_hz: float, resol_per_px: float) -> NDArray:
-    mv_vec = np.diff(pos, axis=0)
-    spd = freq_in_hz * resol_per_px * linalg.norm(mv_vec, axis=1)
+def loc_to_spd(loc: ArrayLike, freq_in_hz: float, resol_per_px: float) -> NDArray:
+    vec = np.diff(loc, axis=0)
+    dist = resol_per_px * linalg.norm(vec, axis=1)
+    spd = freq_in_hz * dist
     return spd
 
 def to_convex(ang: NDArray) -> NDArray:
     return (ang + math.pi) % (2 * math.pi) - math.pi
 
-def pos_to_ang_vel(pos: ArrayLike, freq_in_hz: float) -> NDArray:
-    mv_vec = np.diff(pos, axis=0)
-    ang = to_convex(np.diff(np.arctan2(mv_vec[:, 1], mv_vec[:, 0])))
+def loc_to_ang_vel(loc: ArrayLike, freq_in_hz: float) -> NDArray:
+    vec = np.diff(loc, axis=0)
+    dir = np.arctan2(vec[:, 1], vec[:, 0])
+    ang = to_convex(np.diff(dir))
     ang_vel = freq_in_hz * ang
     return ang_vel
