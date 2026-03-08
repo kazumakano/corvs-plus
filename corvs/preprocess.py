c0bf8155427a7b82e39ba6c8c93caacb2f258fe1
kazumakano <kazumakano@outlook.jp>
2026-03-08 15:22:22 +0900

    update preprocess

--- corvs/preprocess.py
+++ corvs/preprocess.py
@@ -1,13 +1,16 @@
 import math
-from typing import TypeVar, overload
+from typing import Literal, TypeVar, overload
 import numpy as np
 import pandas as pd
+import torch
 from numpy import linalg
 from numpy.typing import ArrayLike, NDArray
+from pandas.core.groupby import DataFrameGroupBy
+from torch.nn.utils import rnn
 
 
-def seg_by_timeout(data: pd.DataFrame, timeout_in_sec: float) -> list[pd.DataFrame]:
-    return [d for _, d in data.groupby(by=(data["time"].diff() > timeout_in_sec).cumsum(), sort=False)]
+def seg_by_timeout(data: pd.DataFrame, timeout_in_sec: float) -> DataFrameGroupBy[int, Literal[True]]:
+    return data.groupby(by=(data["time"].diff() > timeout_in_sec).cumsum(), sort=False)
 
 def loc_to_spd(loc: ArrayLike, freq_in_hz: float, resol_per_px: float) -> NDArray:
     vec = np.diff(loc, axis=0)
@@ -44,3 +47,10 @@ def sync(*args: float | ArrayLike) -> tuple[NDArray[np.float64], ...]:
     val_list = [np.interp(time, t, v) for t, v in zip(time_list, val_list)]
 
     return time, *val_list
+
+def pad(seqs: torch.Tensor | list[torch.Tensor], max_len: int, batch_first: bool = False, pad_val: float = 0) -> torch.Tensor:
+    seqs = rnn.pad_sequence(seqs, batch_first=batch_first, padding_value=pad_val)
+    if batch_first:
+        return torch.cat((seqs, torch.full((len(seqs), max_len - seqs.shape[1], *seqs.shape[2:]), pad_val, dtype=seqs.dtype, device=seqs.device, requires_grad=seqs.requires_grad)), dim=1)
+    else:
+        return torch.cat((seqs, torch.full((max_len - len(seqs), *seqs.shape[1:]), pad_val, dtype=seqs.dtype, device=seqs.device, requires_grad=seqs.requires_grad)), dim=0)
