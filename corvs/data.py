53d159e35e2b915c4c57fc7699d5276253735371
kazumakano <kazumakano@outlook.jp>
2026-03-08 16:39:02 +0900

    update predict dataset

--- corvs/data.py
+++ corvs/data.py
@@ -1,11 +1,21 @@
 from datetime import datetime
 from os import PathLike
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
+import numpy as np
 import pandas as pd
+import torch
 from lightning import pytorch as L
+from numpy import linalg
+from omegaconf import DictConfig
+from scipy import ndimage
+from scipy.interpolate import interp1d
 from torch.utils import data
+from corvs import preprocess
 
+TRAJ_FREQ = 2.5
+TRAJ_RESOL = 0.01
+SENSOR_FREQ = 100
 
 class CorVSDataset(data.Dataset):
     ...
@@ -14,7 +24,9 @@ class CorVSDataModule(L.LightningDataModule):
     ...
 
 class CorVSPredictDataset(data.Dataset):
-    def __init__(self, path: PathLike, traj_track_id: int, sensor_worker_id: int, start: Optional[float | datetime] = None, stop: Optional[float | datetime] = None) -> None:
+    def __init__(self, path: PathLike, traj_track_id: int, sensor_worker_id: int, hparams: dict[str, Any] | DictConfig, start: Optional[float | datetime] = None, stop: Optional[float | datetime] = None) -> None:
+        self.win_len, self.win_stride = hparams["win_len"], hparams["win_stride"]
+
         if isinstance(start, datetime):
             start = start.timestamp()
         if isinstance(stop, datetime):
@@ -22,6 +34,34 @@ class CorVSPredictDataset(data.Dataset):
 
         traj_data = self._load_traj_data(Path(path) / "trajectory", traj_track_id, start, stop)
         sensor_data = self._load_sensor_data(Path(path) / "sensor", sensor_worker_id, start, stop)
+
+        self.time: list[torch.LongTensor] = []
+        self.traj_feat: list[torch.FloatTensor] = []
+        self.sensor_feat: list[torch.FloatTensor] = []
+        self.map: list[tuple[int, int, int]] = []
+        if len(sensor_data) / SENSOR_FREQ > hparams["min_input_len"] / hparams["freq"]:
+            meas = ndimage.gaussian_filter1d(np.column_stack((linalg.norm(sensor_data[["linacc_x", "linacc_y", "linacc_z"]], axis=1), sensor_data[["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"]])), axis=0)
+
+            for i, td in preprocess.seg_by_timeout(traj_data, 5):
+                traj_time = np.arange(td.iloc[0]["time"], td.iloc[-1]["time"], step=1 / TRAJ_FREQ, dtype=np.float64)
+                if len(traj_time) / TRAJ_FREQ > hparams["min_input_len"] / hparams["freq"]:
+                    loc = interp1d(td["time"], td[["x", "y"]], axis=0, copy=False, assume_sorted=True)(traj_time)
+                    spd = ndimage.gaussian_filter1d(preprocess.loc_to_spd(loc, TRAJ_FREQ, TRAJ_RESOL), TRAJ_FREQ / 5)
+                    ang_vel = ndimage.gaussian_filter1d(preprocess.loc_to_ang_vel(loc, TRAJ_FREQ), TRAJ_FREQ / 5)
+
+                    time, spd, ang_vel, meas = preprocess.sync(traj_time[:-1] + 0.5 / TRAJ_FREQ, spd, traj_time[1:-1], ang_vel, sensor_data["time"], meas, hparams["freq"])
+                    self.traj_feat.append(torch.from_numpy(np.column_stack((spd.astype(np.float32), ang_vel.astype(np.float32)))))
+                    self.sensor_feat.append(torch.from_numpy(meas.astype(np.float32)))
+
+                    if len(time) < self.win_len:
+                        win_num = 1
+                    else:
+                        win_num = (len(time) - self.win_len) // self.win_stride + 1
+                    self.time.append(torch.empty(win_num, dtype=torch.float64))
+                    for j in range(win_num):
+                        valid_len = min(len(time) - j * self.win_stride, self.win_len)
+                        self.map.append((i, j, valid_len))
+                        self.time[-1][j] = time[j * self.win_stride]
 
     @staticmethod
     def _load_traj_data(path: Path, traj_track_id: int, start: float | None, stop: float | None) -> pd.DataFrame:
@@ -47,3 +87,16 @@ class CorVSPredictDataset(data.Dataset):
             sensor_data = sensor_data[sensor_data["time"] >= start]
         if stop is not None:
             sensor_data = sensor_data[sensor_data["time"] < stop]
+
+        return sensor_data
+
+    def __getitem__(self, idx: int) -> tuple[torch.LongTensor, torch.FloatTensor, torch.FloatTensor, torch.BoolTensor]:
+        return (
+            self.time[self.map[idx][0]][self.map[idx][1]],
+            preprocess.pad(self.traj_feat[self.map[idx][0]][self.map[idx][1] * self.win_stride:self.map[idx][1] * self.win_stride + self.win_len].unsqueeze(0), self.win_len, True).squeeze(dim=0),
+            preprocess.pad(self.sensor_feat[self.map[idx][0]][self.map[idx][1] * self.win_stride:self.map[idx][1] * self.win_stride + self.win_len].unsqueeze(0), self.win_len, True).squeeze(dim=0),
+            torch.arange(self.win_len, dtype=torch.int32) < self.map[idx][2]
+        )
+
+    def __len__(self) -> int:
+        return len(self.map)
