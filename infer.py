5ea1cdaeb13d0e03da959dda3fcd990782a0f805
kazumakano <kazumakano@outlook.jp>
2026-03-10 01:33:19 +0900

    update help message

--- infer.py
+++ infer.py
@@ -39,8 +39,8 @@ if __name__ == "__main__":
     parser.add_argument("-w", "--weight_path", default="dataset/model.safetensors", help="path to model weight file")
     parser.add_argument("-t", "--traj_track_id", type=int, required=True, help="track ID of trajectory", metavar="TRACK_ID")
     parser.add_argument("-s", "--sensor_worker_id", type=int, required=True, help="worker ID of sensor measurements", metavar="WORKER_ID")
-    parser.add_argument("--from", help="start datetime", metavar="DATETIME", dest="from_")
-    parser.add_argument("--to", help="end datetime", metavar="DATETIME")
+    parser.add_argument("--from", help="start datetime in JST", metavar="DATETIME", dest="from_")
+    parser.add_argument("--to", help="end datetime in JST", metavar="DATETIME")
     args = parser.parse_args()
 
     infer(args.data_path, args.param_path, args.weight_path, args.traj_track_id, args.sensor_worker_id, args.from_, args.to)
