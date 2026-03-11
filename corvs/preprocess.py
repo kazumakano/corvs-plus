ef73e5e5e86cc9b071ca68beb57dad4e038d1d51
kazumakano <kazumakano@outlook.jp>
2026-03-12 08:15:53 +0900

    minor update

--- corvs/preprocess.py
+++ corvs/preprocess.py
@@ -43,8 +43,8 @@ def sync(time_1: ArrayLike, val_1: ArrayLike, time_2: ArrayLike, val_2: ArrayLik
     ...
 
 def sync(*args: float | ArrayLike) -> tuple[NDArray[np.float64], ...]:
-    time_list = [np.asanyarray(a) for a in args[:-1:2]]
-    val_list = [a for a in args[1::2]]
+    time_list = map(np.asanyarray, args[:-1:2])
+    val_list = args[1::2]
     freq = args[-1]
 
     time = np.arange(max(t[0] for t in time_list), min(t[-1] for t in time_list), step=1 / freq, dtype=np.float64)
