d083d74b60d44718da4a04e8288ba3b0973d199b
kazumakano <kazumakano@outlook.jp>
2026-03-24 03:01:09 +0900

    update utils

--- corvs/utils.py
+++ corvs/utils.py
@@ -1,9 +1,20 @@
 import zoneinfo
 from datetime import datetime, tzinfo
+from os import PathLike
 from typing import Optional
 from dateutil import parser
 
 jst = zoneinfo.ZoneInfo("Asia/Tokyo")
 
-def str_to_datetime(dt: str, tzinfo: Optional[tzinfo] = None) -> datetime:
-    return parser.parse(dt).replace(tzinfo=tzinfo)
+def any_to_unix(dt: float | str | datetime, tzinfo: Optional[tzinfo] = None) -> float:
+    if isinstance(dt, str):
+        dt = parser.parse(dt).replace(tzinfo=tzinfo)
+    if isinstance(dt, datetime):
+        if dt.tzinfo is None:
+            dt = dt.replace(tzinfo=tzinfo)
+        dt = dt.timestamp()
+    return dt
+
+def save_txt(data: str, path: PathLike) -> None:
+    with open(path, mode="w") as f:
+        f.write(data)
