e395395e7de4a66c643ad569a2bb9ec158261d5c
kazumakano <kazumakano@outlook.jp>
2026-03-25 15:53:49 +0900

    minor update

--- train.py
+++ train.py
@@ -12,11 +12,11 @@ from corvs.callbacks import ArgsWriter, BestMetricsWriter, ErrWriter
 
 def train(data_path: PathLike, split_path: PathLike, param_path: PathLike, exp_name: Optional[str] = None, start: Optional[float | str | datetime] = None, stop: Optional[float | str | datetime] = None, seed: Optional[float] = None) -> None:
     torch.set_float32_matmul_precision("high")
-    track_ids = tuple(OmegaConf.load(split_path).values())
+    split = OmegaConf.load(split_path)
     hparams = OmegaConf.load(param_path)
 
     model = CorVSNetFitter(hparams, CorVSFitDataset)
-    datamodule = CorVSFitDataModule(data_path, hparams, track_ids, start=start, stop=stop, seed=seed)
+    datamodule = CorVSFitDataModule(data_path, hparams, (split["train"], split["val"], split["test"]), start=start, stop=stop, seed=seed)
 
     cbs = [
         ArgsWriter(data_path=data_path, split_path=split_path, param_path=param_path, start=start, stop=stop, seed=seed),
